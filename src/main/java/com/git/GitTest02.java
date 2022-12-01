55d16553a0af5b79a8a58974c46c03f2966fe712
Lin <Lin@sw.com>
2022-12-01 16:12:43 +0800

    Git2 commit02

--- src/main/java/com/git/GitTest02.java
+++ src/main/java/com/git/GitTest02.java
@@ -3,5 +3,6 @@ package com.git;
 public class GitTest02 {
     public static void main(String[] args) {
         System.out.println("hello,Git2");
+        System.out.println("hello,Git2.1");
     }
 }
