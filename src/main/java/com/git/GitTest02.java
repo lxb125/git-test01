f94063ef0ecd34aea1071d840007dfb6b3c11fc2
lxb125 <119561399+lxb125@users.noreply.github.com>
2022-12-01 17:13:46 +0800

    pull commit

--- src/main/java/com/git/GitTest02.java
+++ src/main/java/com/git/GitTest02.java
@@ -4,5 +4,6 @@ public class GitTest02 {
     public static void main(String[] args) {
         System.out.println("hello,Git2");
         System.out.println("hello,Git2.1");
+        System.out.println("hello,pull");
     }
 }
